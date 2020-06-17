2bb0cab8734febe4637bbecc1c35e440cac90d9f
Sören Maschmann <soeren@maschmann.online>
2020-06-17 10:20:53 +0200

    Interface

--- src/web.ts
+++ src/web.ts
@@ -1,4 +1,4 @@
-import { WebPlugin } from '@capacitor/core';
+/*import { WebPlugin } from '@capacitor/core';
 import { Own2MeshOkLokPluginPlugin } from './definitions';
 
 export class Own2MeshOkLokPluginWeb extends WebPlugin implements Own2MeshOkLokPluginPlugin {
@@ -9,9 +9,24 @@ export class Own2MeshOkLokPluginWeb extends WebPlugin implements Own2MeshOkLokPl
     });
   }
 
-  async echo(options: { value: string }): Promise<{value: string}> {
-    console.log('ECHO', options);
-    return options;
+  async open(options: { id: string, secret: string, pw: string }): Promise<{ opened: boolean }> {
+    console.log('Function not available in this context (Web).');
+    return false;
+  }
+
+  async close(options: { id: string, secret: string }): Promise<{ closed: boolean }> {
+    console.log("Function not available in this context (Web).");
+    return false;
+  }
+
+  async battery_status(options: { id: string, secret: string }): Promise<{ percentage: number }> {
+    console.log("Function not available in this context (Web).");
+    return 0;
+  }
+
+  async lock_status(options: { id: string, secret: string }): Promise<{ locked: boolean }> {
+    console.log("Function not available in this context (Web).");
+    return false;
   }
 }
 
@@ -21,3 +36,4 @@ export { Own2MeshOkLokPlugin };
 
 import { registerWebPlugin } from '@capacitor/core';
 registerWebPlugin(Own2MeshOkLokPlugin);
+*/
