c109f4d848bc4e65fd8e86f0dffc49157f51e587
Marcello Alte <marcello.alte@hs-osnabrueck.de>
2020-08-16 18:28:34 +0200

    definitions update

--- src/definitions.ts
+++ src/definitions.ts
@@ -6,11 +6,14 @@ declare module '@capacitor/core'{
 }
 
 export interface Own2MeshOkLokPlugin {
-  echo(options: { value: string }): Promise<{value: string}>;
-  
-  open(options: { address: string, secret: string, pw: string }): Promise<{ opened: boolean }>;
-  close(options: { address: string, secret: string }): Promise<{ closed: boolean }>;
-  battery_status(options: { address: string, secret: string }): Promise<{ percentage: number }>;
-  lock_status(options: { address: string, secret: string }): Promise<{ locked: boolean }>;
+  echo(options: { value: string }): Promise<{ value: string }>;
+
+  open(options: { name: string, address: string, secret: string, pw: string }): Promise<{ opened: boolean }>;
+
+  close(options: { name: string, address: string, secret: string }): Promise<{ closed: boolean }>;
+
+  battery_status(options: { name: string, address: string, secret: string }): Promise<{ percentage: number }>;
+
+  lock_status(options: { name: string, address: string, secret: string }): Promise<{ locked: boolean }>;
 }
 
