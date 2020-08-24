472171152be27e6a346a7d62b860e945c63e907b
Nils Geschwinde <nils.geschwinde@newroom-media.de>
2020-08-24 04:14:38 +0200

    remove log

--- android/src/main/java/de/own2mesh/plugin/oklok/ByteArrayUtils.java
+++ android/src/main/java/de/own2mesh/plugin/oklok/ByteArrayUtils.java
@@ -48,7 +48,7 @@ public class ByteArrayUtils {
         for (int i = 0; i < array.length(); i++) {
             hexString += array.getString(i).substring(2);
         }
-        Log.i("HexString", hexString);
+        // Log.i("HexString", hexString);
         return hexString;
     }
 }
